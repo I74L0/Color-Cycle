31e35e422596430af5318a6b5e2d4671fd7453bc
I74L0 <i7alog4m3r1@gmail.com>
2023-02-16 12:02:14 -0300

    Style ready

---
+++ wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿let Started = false;
+
+document.getElementById('button').addEventListener("click", function () {
+    Started = !Started;
+    if (Started) {
+        this.innerHTML = "Stop";
+        Change_Color();
+    } else {
+        this.innerHTML = "Start";
+    }
+});
+
+function Change_Color() {
+    let red = document.getElementById("red").value;
+    let green = document.getElementById("green").value;
+    let blue = document.getElementById("blue").value;
+    let t = "#" + red + green + blue;
+    document.documentElement.style.setProperty('--box-color', t);
+}
