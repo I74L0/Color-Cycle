1b313acf9af98a130debbdcbf0e4d562fb525198
I74L0 <i7alog4m3r1@gmail.com>
2023-02-17 10:44:17 -0300

    Funcionality ready

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,15 +1,18 @@
 ﻿var Started = false, interval;
 
+// Start or stop the program
 document.getElementById('button').addEventListener("click", function () {
     Started = !Started;
-
     Prevent_Change(Started);
+
+    // Start the program
     if (Started) {
         let red = document.getElementById("red").value;
         let green = document.getElementById("green").value;
         let blue = document.getElementById("blue").value;
         let color = "#" + red + green + blue;
 
+        // Check if the hexadecimal number is valid
         if (/^#([0-9A-F]{3}){1,2}$/i.test(color)) {
             document.getElementById("color").value = color;
             document.documentElement.style.setProperty('--box-color', color);
@@ -22,31 +25,66 @@ document.getElementById('button').addEventListener("click", function () {
             this.innerHTML = "Start";
             alert("invalid hexadecimal");
         }
-    } else {
+    }
+    // Stop the program
+    else {
         this.innerHTML = "Start";
         clearInterval(interval);
     }
 });
 
+// Prevent the inputs to be changed when the program is started
 function Prevent_Change(Started) {
-    let array = document.querySelectorAll(".inputs input");
+    let array = document.querySelectorAll(".inputs input:not(#color)");
     array.forEach(element => {
         element.readOnly = Started;
     });
 }
 
-function Change_Color(t_red, green, blue) {
+// Change the RGB color
+function Change_Color(red, green, blue) {
+
+    // Get increment value
     let num_red = parseInt(document.getElementById("red_incr").value);
     let num_green = parseInt(document.getElementById("green_incr").value);
     let num_blue = parseInt(document.getElementById("blue_incr").value);
 
-    let red = parseInt(t_red);
-
     interval = setInterval(function () {
-        red = parseInt(red, 16);
-        red = red + num_red;
-        red = red.toString(16);
 
+        // Pass hex to decimal
+        red = parseInt(red, 16);
+        green = parseInt(green, 16);
+        blue = parseInt(blue, 16);
+
+        // Increment decimal
+        red = (red + num_red) % 256;
+        green = (green + num_green) % 256;
+        blue = (blue + num_blue) % 256;
+
+        // Pass decimal back to hex
+        red = red.toString(16);
+        green = green.toString(16);
+        blue = blue.toString(16);
+
+        // Add zero if length is 1
+        if (red.length < 2) {
+            red = "0" + red;
+        }
+        if (green.length < 2) {
+            green = "0" + green;
+        }
+        if (blue.length < 2) {
+            blue = "0" + blue;
+        }
+
+        // Display in input box
         document.getElementById("red").value = red;
-    }, 1000);
+        document.getElementById("green").value = green;
+        document.getElementById("blue").value = blue;
+
+        // Change in the input and box color
+        let color = "#" + red + green + blue;
+        document.getElementById("color").value = color;
+        document.documentElement.style.setProperty('--box-color', color);
+    }, document.getElementById("interval").value);
 }
